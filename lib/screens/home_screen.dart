c1f6b9a55e253d875f56b472ce20d7e0dd3db250
Mojtaba Ibrahim <72887802+Mojtaba-Ibrahim@users.noreply.github.com>
2022-02-24 16:22:06 +0200

    changed text in homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Test"),
+        title: Text("Test2"),
       ),
       body: SafeArea(
           child: Column(
